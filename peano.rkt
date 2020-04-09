fd219282879c4237eaf154059bef44a74600deb3
Alan H <TheAspiringHacker@protonmail.com>
2020-04-08 21:59:12 -0400

    Define equality for Peano

--- peano.rkt
+++ peano.rkt
@@ -2,28 +2,41 @@
 (require redex)
 
 (define-language Peano
-  (e ::= Z (S e)))
+  (m n ::= Z (S n)))
 
 (define-judgment-form Peano
-  #:contract (is-nat e)
+  #:contract (is-nat n)
   [---------- "Z-nat"
    (is-nat Z)]
 
-  [(is-nat e)
+  [(is-nat n)
    -------------- "S-nat"
-   (is-nat (S e))])
+   (is-nat (S n))])
+
+(define-judgment-form Peano
+  #:contract (equals n n)
+  #:mode (equals I O)
+  [------------ "Z-eq"
+   (equals Z Z)]
+
+  [(equals m n)
+   -------------------- "S-eq"
+   (equals (S m) (S n))])
 
 (define Z-is-nat
   (derivation
-  '(is-nat Z)
-  "Z-nat"
-  '()))
+   '(is-nat Z)
+   "Z-nat"
+   '()))
 
 (define SZ-is-nat
   (derivation
-  '(is-nat (S Z))
-  "S-nat"
-  `(,Z-is-nat)))
+   '(is-nat (S Z))
+   "S-nat"
+   `(,Z-is-nat)))
 
 (test-judgment-holds is-nat Z-is-nat)
 (test-judgment-holds is-nat SZ-is-nat)
+
+(test-judgment-holds (equals Z Z))
+(test-judgment-holds (equals (S Z) (S Z)))
